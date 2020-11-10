5aa3827ff00e6ac894dd9e72760a4af3c1e87a98
英士 <comu2e@gmail.com>
2020-11-11 00:16:04 +0900

    登録画面の作成 ImageInput.vueで写真をドラッグアンドドロップできる機能付け足す。（今後）

---
+++ resources/views/user_detail_view.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+{{--foreachでuserを詳細表示--}}
+{{--foreachでuserをユーザーの道具表示--}}
+
+{{--<!--    --><?php>//--}}
+{{--//    $element_title=array['カットギア','シェルターギア','火熾しギア','コンテナギア','快適化ギア'];--}}
+{{--//    $element_descript=array['食材・木材を思い通りにカット','雨風から体を守る空間を創る','明かりと暖を楽しむ','ギアの整理整頓','自然の中でも快適に'];--}}
+{{--//--}}
+{{--//    <?>--}}
+
+<div>
+{{--    @foreach ($element_title as $item )--}}
+{{--    <li>{{$item}}</li>--}}
+{{--    @endforeach--}}
+</div>
+@endsection

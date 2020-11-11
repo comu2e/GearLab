f2a21c79f2dcb37a63f44c8b4fb7f1c65191347d
英士 <comu2e@gmail.com>
2020-11-12 05:30:14 +0900

    UserConntrollerの実装 Userの一覧をusers.index Userの詳細をusers.show で表示する。

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 use App\Models\User;
 
 class UserController extends Controller
 {
-//    Repositoryパターンで書き直す
-    public static function index()
+
+    public function index()
     {
-        return User::all();
+        $users =User::all();
+        return view('users/index',compact('users'));
+
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+{
+        //
+     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categories = array('カットギア','シェルターギア','','火熾しギア','コンテナギア','快適化ギア');
+
+        $user = User::find($id);
+        return view('users.show', compact('user','categories'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }

6d59da5f06d338feec0cff52660e25d61a0987c6
英士 <comu2e@gmail.com>
2020-11-12 23:35:27 +0900

    ここまででできたこと GearController.phpのcategoryで 複数条件検索(user_id,gear_category)で
    category_index.blade.phpに値を渡す
    
    できていないこと
    gear_categorizedのimage_urlを取得してforループ回せていない

--- app/Http/Controllers/GearController.php
+++ app/Http/Controllers/GearController.php
@@ -10,6 +10,18 @@ use App\Models\Gear;
 class GearController extends Controller
 {
 
+    public function category_index()
+    {
+        $gear_category = 1;
+        $gears =Gear::all();
+
+        $gear_categorized =$gears->where('gear_category',$gear_category)->where('user_id',1)->all();
+        var_dump($gear_categorized);
+        return view('gears/category_index',compact('gear_categorized'));
+
+
+    }
+
     public function index()
     {
         $gears =Gear::all();

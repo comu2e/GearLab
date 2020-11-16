1f8da45f0a9e04f20b555ee6b3bc07ba1bff902e
英士 <comu2e@gmail.com>
2020-11-16 21:31:21 +0900

    グローバル変数$categoryを使ってセレクトボックスをつくる

--- resources/views/users/category_index.blade.php
+++ resources/views/users/category_index.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$user}}
     {{$gear_category}}
-    {{$gear_categorized}}
     @foreach($gear_categorized as $gear)
       <div class="d-flex justify-content-center">  {{($gear->$gear_category)}}</div>
+      <div class="d-flex justify-content-center">  {{($gear->$image_url)}}</div>
     @endforeach
 
 @endsection

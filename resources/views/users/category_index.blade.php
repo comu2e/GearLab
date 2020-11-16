7d15cfc6a63264bebda824efa7fd28876eec1ee2
英士 <comu2e@gmail.com>
2020-11-16 21:35:55 +0900

    ギアの表示とimage_urlの表示

--- resources/views/users/category_index.blade.php
+++ resources/views/users/category_index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     {{$gear_category}}
     @foreach($gear_categorized as $gear)
-      <div class="d-flex justify-content-center">  {{($gear->$gear_category)}}</div>
-      <div class="d-flex justify-content-center">  {{($gear->$image_url)}}</div>
+      <div class="d-flex justify-content-center">  {{($gear->gear_category)}}</div>
+      <div class="d-flex justify-content-center">  {{$gear->image_url}}</div>
     @endforeach
 
 @endsection

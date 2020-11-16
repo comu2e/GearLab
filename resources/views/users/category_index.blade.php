aa280cfee392f152b73c8f5fd4f8005bd2139588
英士 <comu2e@gmail.com>
2020-11-16 23:33:49 +0900

    画像のaset追加 追加画面の編集

--- resources/views/users/category_index.blade.php
+++ resources/views/users/category_index.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     {{$gear_category}}
     @foreach($gear_categorized as $gear)
+
       <div class="d-flex justify-content-center">  {{($gear->gear_category)}}</div>
-      <div class="d-flex justify-content-center">  {{$gear->image_url}}</div>
+      <img src={{$gear->image_url}} width="500" height="500">
+
     @endforeach
 
 @endsection

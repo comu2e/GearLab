7c9fd66f17f0b170be6de3600e1fa00083102d82
英士 <comu2e@gmail.com>
2020-11-17 23:12:24 +0900

    画像位置を真ん中に揃える

--- resources/views/users/category_index.blade.php
+++ resources/views/users/category_index.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$gear_category}}
+    <div class="d-flex justify-content-center">{{$gear_category}} </div>
     @foreach($gear_categorized as $gear)
+<div class="container">
+    <div class="d-flex justify-content-center">
+        <img src={{$gear->image_url}} width="500" height="500">
+    </div>
+    <div class="d-flex justify-content-center">
 
-      <div class="d-flex justify-content-center">  {{($gear->gear_category)}}</div>
-      <img src={{$gear->image_url}} width="500" height="500">
-      <div>
-          <a href="{{ route('gear_edit')}}/id={{ $gear->id }}" class="btn btn-primary">
-              {{ __('Edit') }}
-          </a>
-          {{-- 削除ボタンは後で正式なものに置き換えます --}}
-          <a href="{{ url('gears/{gear}') }}" class="btn btn-danger">
-              {{ __('Delete') }}
-          </a>
-      </div>
+        <a href="{{ route('gear_edit')}}/id?{{$gear->id}}" class="btn btn-primary">
+            {{ __('Edit') }}
+        </a>
+        {{-- 削除ボタンは後で正式なものに置き換えます --}}
+        <a href="{{ url('gears/{gear}') }}" class="btn btn-danger">
+            {{ __('Delete') }}
+        </a>
+    </div>
+
+</div>
+
     @endforeach
 
 @endsection

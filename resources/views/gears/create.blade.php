842307a40d12628c60b8371a323f4b6e2b1a1674
英士 <comu2e@gmail.com>
2020-11-17 21:50:26 +0900

    フォームにbootstrap導入して見た目整える

--- resources/views/gears/create.blade.php
+++ resources/views/gears/create.blade.php
@@ -1,55 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center">
 
-    <form method="POST" action="/gears"enctype="multipart/form-data">
-        @csrf
-        <ul>
-            <li>カテゴリ
+        <form method="POST" action="/gears" enctype="multipart/form-data">
+
+            @csrf
+            <div class="form-group">
+
+                <label>カテゴリ</label>
+
                 {{Form::select('gear_category',$categories)}}
 
-            </li>
+            </div>
 
-
-            <li>ギアの名前<input name="gear_name" type="text"> </li>
+            <div class="form-group">
+                <label>ギアの名前</label>
+                <input name="gear_name" type="text">
                 <div class="text-danger">
-                @error('gear_name')
-                {{$message}}
-                @enderror
+                    @error('gear_name')
+                    {{$message}}
+                    @enderror
                 </div>
 
-            <li>ギアのメーカー名 <input name="maker_name" type="text"></li>
-        {{--        入力値をデータベースにまだ渡しない--}}
+            </div>
+            <div class="form-group">
+
+                <div class="form-group">ギアのメーカー名 <input name="maker_name" type="text">
+                </div>
+            </div>
             <div class="text-danger">
                 @error('maker_name')
                 {{$message}}
                 @enderror
             </div>
-        <li>ギアの使用場面 <input name="situation"type="text"></li>
+            <div class="form-group">ギアの使用場面
+                <input name="situation" type="text">
+            </div>
 
-        <li>お気に入りポイント<input  name="content"type="text"> </li>
-{{--        入力値をデータベースにまだ渡しない--}}
+            <div class="form-group">お気に入りポイント<input name="content" type="text"></div>
+            {{--        入力値をデータベースにまだ渡しない--}}
             <div class="text-danger">
                 @error('content')
                 {{$message}}
                 @enderror
             </div>
-        <li>購入場所<input name="place"type="text"></li>
-        <li>ギアの写真
-            <input name="image_url"type="file" >
+            ギアの写真
+
+            <input name="image_url" type="file">
             <div class="text-danger">
                 @error('image_url')
                 {{$message}}
                 @enderror
             </div>
-        </li>
-    </ul>
 
-        <input type="submit" class="btn btn-primary" value="投稿"/>
+            <input type="submit" class="btn btn-primary" value="投稿"/>
 
-    </form>
+        </form>
 
-</div>
+    </div>
 @endsection
 

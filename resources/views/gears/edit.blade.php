d2093af23c84217f703c6a9e64096f06f5f2f991
英士 <comu2e@gmail.com>
2020-11-18 20:40:43 +0900

    編集画面のplaceholder設定

--- resources/views/gears/edit.blade.php
+++ resources/views/gears/edit.blade.php
@@ -1,23 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="d-flex justify-content-center">
 
+        <form method="POST" action="/gears" enctype="multipart/form-data">
 
-    <div class="container">
-        <div class="d-flex justify-content-center">
-            <img src={{$gear->image_url}} width="500" height="500">
-        </div>
-        <div class="d-flex justify-content-center">
-            {{--        <a href="{{ route('gear_edit') }}">--}}
+            @csrf
+            <div class="form-group">
 
-            <a href="{{ url("edit/$gear->gear_id") }}" class="btn btn-primary">
-                {{ __('Edit') }}
-            </a>
-            {{-- 削除ボタンは後で正式なものに置き換えます --}}
-            <a href="{{ url('gears/{gear}') }}" class="btn btn-danger">
-                {{ __('Delete') }}
-            </a>
-        </div>
+                <label>カテゴリ</label>
+
+                {{Form::select('gear_category',
+$categories,null,['class' => 'form-hoge',  'placeholder' => $gear->gear_category]) }}
+
+            </div>
+
+            <div class="form-group">
+                <label>ギアの名前</label>
+                <input name="gear_name" type="text" placeholder="{{$gear->gear_name}}">
+                <div class="text-danger">
+                    @error('gear_name')
+                    {{$message}}
+                    @enderror
+                </div>
+
+            </div>
+            <div class="form-group">
+
+                <div class="form-group">
+                    <label>ギアのメーカー名</label>
+                    <input name="maker_name" type="text" placeholder="{{$gear->maker_name}}">
+                </div>
+            </div>
+            <div class="text-danger">
+                @error('maker_name')
+                {{$message}}
+                @enderror
+            </div>
+
+            <div class="form-group">
+                お気に入りポイント<input name="content" type="text"  placeholder="{{$gear->content}}"></div>
+            {{--        入力値をデータベースにまだ渡しない--}}
+            <div class="text-danger">
+                @error('content')
+                {{$message}}
+                @enderror
+            </div>
+
+            <div class="form-group">
+
+            <label for="">
+                ギアの写真
+            </label>
+            <img src="{{$gear->image_url}}" width=40%>
+                <div class="form-group">
+
+            <input name="image_url" type="file">
+                </div>
+            <div class="text-danger">
+                @error('image_url')
+                {{$message}}
+                @enderror
+            </div>
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="投稿"/>
+        </form>
 
     </div>
 

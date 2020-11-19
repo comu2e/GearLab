e90a44e42477b00d0e6756c5341a6418727109e8
英士 <comu2e@gmail.com>
2020-11-19 21:48:46 +0900

    登録ボタン修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -28,12 +28,14 @@
             </ul>
             {{--   認証されているときのみ投稿できるフォームを出す--}}
 
-            <div class="d-flex justify-content-center">
-                <button type="button" class="btn btn-primary rounded-circle p-0" style="width:5rem;height:5rem;color:white">
-                    <a color=white href = {{ url('gears/create') }}>Add Gear</a>
-                </button>
-            </div>
 
+@guest
+
+@else
+<div class="d-flex justify-content-center">
+
+                    <a href = {{ url('gears/create') }} class="btn btn-primary" >Register Gear</a>
+            </div>@endguest
 
         </div>
     </div>

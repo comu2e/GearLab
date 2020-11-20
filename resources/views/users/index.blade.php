43f16cc5e289d2353c2876234d5678ae9a5fd70c
英士 <comu2e@gmail.com>
2020-11-21 00:38:49 +0900

    ページネーター修正 https://qiita.com/kazuhei/items/7827ecf22ef9fc3a603c が参考になった

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,6 +16,14 @@
 
         </ul>
 
-        {{$users->links()}}
     </div>
+
+@endsection
+
+@section('footer')
+    <div class="d-flex justify-content-center">
+        {{$users->links('pagination::bootstrap-4') }}
+
+    </div>
+
 @endsection

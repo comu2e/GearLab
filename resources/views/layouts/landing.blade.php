120221cf94efa2b4235b3abe93399f38b881ab0f
英士 <comu2e@gmail.com>
2020-11-22 13:13:28 +0900

    ランディングページの文言変更

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -140,17 +140,14 @@
                 </div>
             </div>
         </header>
-        <main class="py-4">
-            @yield('content')
-        </main>
+
     </div>
-    @yield('footer')
     <footer>
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    <h3>Your Logo</h3>
-                    <p>© 2016 Your Company. Designed and Developed by <a target="_blank" href="http://www.themeinthebox.com">ThemeintheBox.com</a></p>
+                    <h3>Gear Lab</h3>
+                    <p>© 2020 Eiji Takahashi. Designed and Developed by <a target="_blank" href="http://www.themeinthebox.com">ThemeintheBox.com</a></p>
                 </div>
                 <div class="col-md-4">
                     <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i><i class="typcn typcn-social-twitter-circular"></i><i class="typcn typcn-social-tumbler-circular"></i><i class="typcn typcn-social-github-circular"></i><i class="typcn typcn-social-dribbble-circular"></i></p>

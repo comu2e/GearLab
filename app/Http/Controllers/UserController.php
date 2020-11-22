9cf53ed644e07f0834ad1923cfcab374107a5871
英士 <comu2e@gmail.com>
2020-11-22 13:13:28 +0900

    ランディングページの文言変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,11 @@ use App\Models\User;
 class UserController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified'])->only(['like', 'unlike']);
+        $this->middleware('auth');
+    }
     public function index()
     {
 //        $users =User::all();

49a0b80486b6175f0ea7ce088d289242a18375cd
英士 <comu2e@gmail.com>
2020-11-22 21:34:15 +0900

    ユーザー一覧の修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,13 +27,16 @@
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ 'GearLab' }}
             </a>
-
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+            <div class="d-flex justify-content-center">
+                <a href={{ url('/users') }} class="btn btn-primary">
+                ユーザ一覧
+                </a>
+            </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
@@ -78,6 +81,19 @@
                                     @csrf
                                 </form>
                             </div>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ 'kok' }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+
                         </li>
                     @endguest
                 </ul>

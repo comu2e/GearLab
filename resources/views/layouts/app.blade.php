4fc89fba9087de03bf019e241162dea138e2106e
英士 <comu2e@gmail.com>
2020-11-22 15:15:02 +0900

    landing pageを整理。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,15 +23,22 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
+
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ 'GearLab' }}
             </a>
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+            <form class="form-inline my-2 my-lg-0 ml-2">
+                <div class="form-group">
+                    <input type="search" class="form-control mr-sm-2" name="search"  value="{{request('search')}}" placeholder="キーワードを入力" aria-label="検索...">
+                </div>
+                <input type="submit" value="検索" class="btn btn-primary">
+            </form>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
@@ -57,7 +64,7 @@
                     @else
                         <div class="d-flex justify-content-center">
 
-                            <a href={{ url('gears/create') }} class="btn btn-primary" >Register Gear</a>
+                            <a href={{ url('gears/create') }} class="btn btn-primary"> Gear <strong> + </strong></a>
                         </div>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"

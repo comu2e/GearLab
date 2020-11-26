d5448e6b862004de0334c9e0d77ab5218e049ab6
英士 <comu2e@gmail.com>
2020-11-26 21:50:31 +0900

    unitテストクラスの作成

--- tests/Feature/GearTest.php
+++ tests/Feature/GearTest.php
@@ -24,31 +24,4 @@ class GearTest extends TestCase
         $response->assertStatus(200);
     }
 
-//    public function testEdit_gear(){
-//
-//        for ($i=1;$i<4;$i++){
-//            $gear = Gear::find($i);
-//
-//            $response = $this->get('/');
-//
-//            $response->assertStatus(200)
-//                ->assertViewIs('layouts.landing', compact('gear')); // 追加
-//        }
-//
-
-//        $response = $this->get('/edit/'.$id);
-//
-//        $response->assertStatus(302)
-//            ->assertViewIs('users.index', compact('gear')); // 追加
-//    }
-//    public function testDestroy(){
-////        $ids = DB::table('gears')->pluck('id');
-////        echo $ids;
-////        foreach ($ids as $id ){
-////            $gears = Gear::find($id);
-////            $this->assertSoftDeleted('gears',$gears);
-////        }
-//        $user = factory(User::class)->create();
-//
-//    }
 }

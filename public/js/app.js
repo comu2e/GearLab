cba4efa744bfa66f8a99d22693198f065a60b585
英士 <comu2e@gmail.com>
2020-11-29 01:49:08 +0900

    SPAのページ追加中 道具一覧、道具追加、表示できるようにするため

--- public/js/app.js
+++ public/js/app.js
@@ -1932,6 +1932,159 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/GearComponent.vue?vue&type=script&lang=js&":
+/*!************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/GearComponent.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "GearComponent",
+  data: function data() {
+    return {
+      message: "",
+      isPush: false,
+      updateForm: false,
+      gears: {},
+      id: "",
+      user_id: "",
+      gear_category: "",
+      gear_name: "",
+      maker_name: "",
+      image_url: "",
+      updated_at: "",
+      created_at: ""
+    };
+  },
+  created: function created() {
+    this.getGear();
+  },
+  methods: {
+    getGear: function getGear() {
+      var _this = this;
+
+      axios.get("/api/gears/").then(function (response) {
+        _this.gears = response.data;
+      })["catch"](function (err) {
+        _this.message = err;
+      });
+    },
+    addGear: function addGear() {
+      var _this2 = this;
+
+      axios.post("/api/gears/", {
+        gear_category: this.gear_category,
+        gear_name: this.gear_name,
+        maker_name: this.maker_name,
+        updated_at: this.updated_at,
+        content: this.content,
+        image_url: this.image_url
+      }).then(function (response) {
+        _this2.getGear();
+
+        _this2.id = "";
+        _this2.user_id = "";
+        _this2.gear_category = "";
+        _this2.maker_name = "";
+        _this2.updated_at = "";
+        _this2.created_at = "";
+        _this2.edited_at = "";
+        _this2.content = "";
+        _this2.image_url = "";
+      })["catch"](function (err) {
+        _this2.message = err;
+      });
+    },
+    // displayUpdate(id, image, content) {
+    //     this.isPush = true;
+    //     this.updateForm = true;
+    //     this.message = "";
+    //     this.updateId = id;
+    //     this.updateTitle = title;
+    //     this.updateAuthor = author;
+    // },
+    // updateCancel() {
+    //     this.isPush = false;
+    //     this.updateForm = false;
+    //     this.message = "";
+    // },
+    // updateBook(updateId, updateContent) {
+    //     axios
+    //         .put("/api/gears/" + updateId, {
+    //             title: this.updateContent,
+    //         })
+    //         .then(response => {
+    //             this.getGear();
+    //             this.isPush = false;
+    //             this.updateForm = false;
+    //             this.message = "";
+    //         })
+    //         .catch(err => {
+    //             this.message = err;
+    //         });
+    // },
+    deleteGear: function deleteGear(id) {
+      var _this3 = this;
+
+      axios["delete"]("/api/gears/" + id).then(function (response) {
+        _this3.getGear();
+
+        _this3.message = "";
+      })["catch"](function (err) {
+        _this3.message = err;
+      });
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/bootstrap/dist/js/bootstrap.js":
 /*!*****************************************************!*\
   !*** ./node_modules/bootstrap/dist/js/bootstrap.js ***!
@@ -37554,6 +37707,117 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a&":
+/*!****************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a& ***!
+  \****************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", [
+    _c("div", [
+      _c("p", [
+        _vm._v("カテゴリ："),
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.gear_category,
+              expression: "gear_category"
+            }
+          ],
+          attrs: { type: "text" },
+          domProps: { value: _vm.gear_category },
+          on: {
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.gear_category = $event.target.value
+            }
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("p", [
+        _vm._v("content："),
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.content,
+              expression: "content"
+            }
+          ],
+          attrs: { type: "text" },
+          domProps: { value: _vm.content },
+          on: {
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.content = $event.target.value
+            }
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("button", { on: { click: _vm.addGear } }, [_vm._v("追加")])
+    ]),
+    _vm._v(" "),
+    _c("div", [
+      _c(
+        "ul",
+        _vm._l(_vm.gears, function(gear) {
+          return _c("li", { key: gear.id }, [
+            _vm._v(
+              "\n                    " +
+                _vm._s(gear.id) +
+                "/" +
+                _vm._s(gear.title) +
+                "/" +
+                _vm._s(gear.gear_category) +
+                "\n"
+            ),
+            _vm._v(" "),
+            _c(
+              "button",
+              {
+                attrs: { disabled: _vm.isPush },
+                on: {
+                  click: function($event) {
+                    return _vm.deleteGear(gear.id)
+                  }
+                }
+              },
+              [_vm._v("\n                        削除\n                    ")]
+            )
+          ])
+        }),
+        0
+      )
+    ]),
+    _vm._v(" "),
+    _vm.message ? _c("p", [_vm._v(_vm._s(_vm.message))]) : _vm._e()
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
 /*!********************************************************************!*\
   !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
@@ -49760,6 +50024,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+Vue.component('gear-component', __webpack_require__(/*! ./components/GearComponent.vue */ "./resources/js/components/GearComponent.vue")["default"]);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -49881,6 +50146,75 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_template_id_299e239e___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_template_id_299e239e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
+/***/ "./resources/js/components/GearComponent.vue":
+/*!***************************************************!*\
+  !*** ./resources/js/components/GearComponent.vue ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./GearComponent.vue?vue&type=template&id=cc68ee5a& */ "./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a&");
+/* harmony import */ var _GearComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./GearComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/GearComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _GearComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/GearComponent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/GearComponent.vue?vue&type=script&lang=js&":
+/*!****************************************************************************!*\
+  !*** ./resources/js/components/GearComponent.vue?vue&type=script&lang=js& ***!
+  \****************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GearComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./GearComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/GearComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GearComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a&":
+/*!**********************************************************************************!*\
+  !*** ./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a& ***!
+  \**********************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./GearComponent.vue?vue&type=template&id=cc68ee5a& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/GearComponent.vue?vue&type=template&id=cc68ee5a&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_GearComponent_vue_vue_type_template_id_cc68ee5a___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
---
+++ resources/js/components/GearComponent.vue
@@ -0,0 +1,139 @@
+<template>
+    <div>
+        <!-- 新規追加フォーム -->
+        <div>
+            <p>カテゴリ：<input type="text" v-model="gear_category" /></p>
+            <p>content：<input type="text" v-model="content" /></p>
+            <button @click="addGear">追加</button>
+        </div>
+
+        <!-- book一覧 -->
+        <div>
+            <ul>
+                <li v-for="gear in gears" :key="gear.id">
+                    {{ gear.id }}/{{ gear.title }}/{{ gear.gear_category }}
+
+                    <button :disabled="isPush" @click="deleteGear(gear.id)">
+                        削除
+                    </button>
+                </li>
+            </ul>
+        </div>
+
+        <!-- 編集フォーム -->
+<!--        <div v-if="updateForm">-->
+<!--            <p>idが{{ updateId }}の編集フォーム</p>-->
+<!--            <p>タイトル：<input type="text" v-model="updateTitle" /></p>-->
+<!--            <p>著者：<input type="text" v-model="updateAuthor" /></p>-->
+<!--&lt;!&ndash;            <button @click="updateBook(updateId, updateTitle, updateAuthor)">&ndash;&gt;-->
+<!--&lt;!&ndash;                編集する&ndash;&gt;-->
+<!--&lt;!&ndash;            </button>&ndash;&gt;-->
+<!--&lt;!&ndash;            <button @click="updateCancel">キャンセル</button>&ndash;&gt;-->
+<!--        </div>-->
+
+        <!-- エラーメッセージ -->
+        <p v-if="message">{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GearComponent",
+    data() {
+        return {
+            message: "",
+            isPush: false,
+            updateForm: false,
+            gears: {},
+            id: "",
+            user_id: "",
+            gear_category: "",
+            gear_name: "",
+            maker_name: "",
+            image_url: "",
+            updated_at: "",
+            created_at: ""
+        };
+    },
+    created: function() {
+        this.getGear();
+    },
+    methods: {
+        getGear() {
+            axios
+                .get("/api/gears/")
+                .then(response => {
+                    this.gears = response.data;
+                })
+                .catch(err => {
+                    this.message = err;
+                });
+        },
+        addGear() {
+            axios
+                .post("/api/gears/", {
+                    gear_category: this.gear_category,
+                    gear_name: this.gear_name,
+                    maker_name: this.maker_name,
+                    updated_at: this.updated_at,
+                    content: this.content,
+                    image_url: this.image_url
+                })
+                .then(response => {
+                    this.getGear();
+                    this.id = "";
+                    this.user_id = "";
+                    this.gear_category = "";
+                    this.maker_name = "";
+                    this.updated_at = "";
+                    this.created_at = "";
+                    this.edited_at = "";
+                    this.content = "";
+                    this.image_url = "";
+                })
+                .catch(err => {
+                    this.message = err;
+                });
+        },
+        // displayUpdate(id, image, content) {
+        //     this.isPush = true;
+        //     this.updateForm = true;
+        //     this.message = "";
+        //     this.updateId = id;
+        //     this.updateTitle = title;
+        //     this.updateAuthor = author;
+        // },
+        // updateCancel() {
+        //     this.isPush = false;
+        //     this.updateForm = false;
+        //     this.message = "";
+        // },
+        // updateBook(updateId, updateContent) {
+        //     axios
+        //         .put("/api/gears/" + updateId, {
+        //             title: this.updateContent,
+        //         })
+        //         .then(response => {
+        //             this.getGear();
+        //             this.isPush = false;
+        //             this.updateForm = false;
+        //             this.message = "";
+        //         })
+        //         .catch(err => {
+        //             this.message = err;
+        //         });
+        // },
+        deleteGear(id) {
+            axios
+                .delete("/api/gears/" + id)
+                .then(response => {
+                    this.getGear();
+                    this.message = "";
+                })
+                .catch(err => {
+                    this.message = err;
+                });
+        }
+    }
+};
+</script>

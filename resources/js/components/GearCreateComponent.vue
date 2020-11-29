73da2e8d8b46a03413dc9f6b3b6dd716006776ff
英士 <comu2e@gmail.com>
2020-11-29 21:23:37 +0900

    create画面ととのいていく

--- resources/js/components/GearCreateComponent.vue
+++ resources/js/components/GearCreateComponent.vue
@@ -1,30 +1,65 @@
 <template>
-    <form v-on:submit.prevent="submit">
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label" for="id">Title</label>
-            <input id="id" v-model="gear.id" class="col-sm-9 form-control" type="text">
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label" for="content">Content</label>
-            <input id="content" v-model="gear.content" class="col-sm-9 form-control" type="text">
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label" for="maker_name">maker_name</label>
-            <input id="maker_name" v-model="gear.maker_name" class="col-sm-9 form-control" type="text">
-        </div>
-        <button class="btn btn-primary" type="submit">Submit</button>
 
-    </form>
+<div class="container">
+    <div align="center">
+        <form v-on:submit.prevent="submit" >
+
+            <div>
+
+
+                <label>カテゴリー</label>
+
+                <select v-model="selected">
+
+                    <option>BackPack</option>
+                    <option>Cut</option>
+                    <option>Shelter</option>
+                    <option>Kitchen</option>
+                    <option>Bonfire</option>
+                </select>
+                {{selected}}
+            </div>
+            <div class="form-group">
+                <label>ギアの名前</label>
+                <input id="gear_name" v-model="gear.gear_name"  type="text">
+            </div>
+            <div class="form-group"><label>内容</label>
+                <input id="content" v-model="gear.content"  type="text">
+            </div>
+            <div class="form-group"><label>メーカー名</label>
+                <input id="maker_name" v-model="gear.maker_name"  type="text">
+            </div>
+
+            <!--                <div>-->
+            <!--                    <input type="file"  v-model="gear.image_url"  name="image_url">-->
+
+            <!--                </div>-->
+            <div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+
+            </div>
+
+
+        </form>
+    </div>
+
+</div>
+
 </template>
 
 <script>
+
 export default {
     name: 'GearCreateComponent',
     data: function () {
         return {
-            gear: {}
+            gear: {},
+            isDrag: null
+
+
         }
     },
+    props: {},
     methods: {
         submit() {
             axios.post('/api/gears', this.gear)
@@ -32,7 +67,8 @@ export default {
                     this.$router.push({name: 'gear.list'});
                 });
         }
-    }
+    },
+
 }
 
 </script>

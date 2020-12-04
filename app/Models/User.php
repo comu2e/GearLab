da61c962fe332f0440f97244d321db3f6ab6efb6
英士 <comu2e@gmail.com>
2020-12-04 09:20:03 +0900

    follow機能の実装 フォローボタンがincludeされない

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,51 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
     use HasApiTokens;
+    public function gears()
+    {
+        return $this->hasMany(Gear::class);
+    }
 
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+
+    public function is_following($userId)
+    {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+
+    public function follow($userId)
+    {
+        // すでにフォロー済みではないか？
+        $existing = $this->is_following($userId);
+        // フォローする相手がユーザ自身ではないか？
+        $myself = $this->id == $userId;
+
+        // フォロー済みではない、かつフォロー相手がユーザ自身ではない場合、フォロー
+        if (!$existing && !$myself) {
+            $this->followings()->attach($userId);
+        }
+    }
+
+    public function unfollow($userId)
+    {
+        // すでにフォロー済みではないか？
+        $existing = $this->is_following($userId);
+        // フォローする相手がユーザ自身ではないか？
+        $myself = $this->id == $userId;
+
+        // すでにフォロー済みならば、フォローを外す
+        if ($existing && !$myself) {
+            $this->followings()->detach($userId);
+        }
+    }
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,31 +19,8 @@ Route::get('/', function () {
     return view('/layouts.landing');
 });
 
-
-
-Route::get('/user_detail/{id?}', function () {
-    return view('user_detail_view')->name('user_detail');
-});
-
-
 Auth::routes();
 
-Route::resource('users','App\Http\Controllers\UserController');
-Route::resource('gears','App\Http\Controllers\GearController');
-
-Route::get('users/{user_id}/gear_category={gear_category}','App\Http\Controllers\GearController@category_index')->name('gear_category');
-
-Route::get('edit/{id}', 'App\Http\Controllers\GearController@edit_gear')->name('gear_edit');
-Route::post('edit/{id}', 'App\Http\Controllers\GearController@update_gear')->name('gear_update');
-//投稿フォームページ
-//Route::group(['middleware' => 'auth'], function() {
-//    Route::get('/gears', 'GearController@showCreateForm')->name('posts.create');
-//    Route::post('/gears', 'GearController@create');
-//});
-//いいね
-Route::get('/gear/like/{id}', 'App\Http\Controllers\GearController@like')->name('gear.like');
-Route::get('/gear/unlike/{id}', 'App\Http\Controllers\GearController@unlike')->name('gear.unlike');
-//管理側
 Route::group(['middleware' => ['auth.admin']], function () {
 
     //管理側トップ
@@ -62,9 +40,41 @@ Route::post('/admin/login', 'App\Http\Controllers\admin\AdminLoginController@log
 
 
 //ルートの保護
-Route::get('profile', function() {
+Route::get('profile', function () {
     // 認証済みのユーザーのみが入れる
 })->middleware('auth');
 
-Route::get('/favorite','App\Http\Controllers\GearController@favorite_gear');
-Route::post('/admin/user/{id}','App\Http\Controllers\GearController@delete');
+Route::get('/favorite', 'App\Http\Controllers\GearController@favorite_gear');
+Route::post('/admin/user/{id}', 'App\Http\Controllers\GearController@delete');
+//フォロー機能
+//ログインしていないユーザーにもフォロー関係を表示できるようにする
+Route::group(['prefix' => 'users/{id}'], function () {
+    Route::get('followings', 'App\Http\Controllers\UserController@followings')->name('followings');
+    Route::get('followers', 'App\Http\Controllers\UsersController@followers')->name('followers');
+});
+//ログインしたときに表示
+Route::group(['middleware' => 'auth'], function () {
+    Route::put('users', 'App\Http\Controllers\UserController@rename')->name('rename');
+
+    Route::get('/user_detail/{id?}', function () {
+        return view('user_detail_view')->name('user_detail');
+    });
+
+    Route::resource('users', 'App\Http\Controllers\UserController');
+    Route::resource('gears', 'App\Http\Controllers\GearController');
+
+    Route::get('users/{user_id}/gear_category={gear_category}', 'App\Http\Controllers\GearController@category_index')->name('gear_category');
+
+    Route::get('edit/{id}', 'App\Http\Controllers\GearController@edit_gear')->name('gear_edit');
+    Route::post('edit/{id}', 'App\Http\Controllers\GearController@update_gear')->name('gear_update');
+
+    //いいね
+    Route::get('/gear/like/{id}', 'App\Http\Controllers\GearController@like')->name('gear.like');
+    Route::get('/gear/unlike/{id}', 'App\Http\Controllers\GearController@unlike')->name('gear.unlike');
+    //管理側
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('unfollow');
+    });
+
+});

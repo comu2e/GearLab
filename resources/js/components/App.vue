4c404865544b3c1f4b52218f3268860ce7453812
英士 <comu2e@gmail.com>
2020-12-05 22:59:36 +0900

    ナビゲーションバーを追加 リロードするとログアウトされてしまう

---
+++ resources/js/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container" >
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark"position="fixed">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link to="/gears" class="nav-link">ギアタイムライン</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/gears/create" class="nav-link">Gear+</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/likes" class="nav-link">いいねしたギア</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/follows" class="nav-link">フォローしているユーザー</router-link>
+                </li>
+            </ul>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>

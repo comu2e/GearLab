f042d7c537a6e2c646240eac42382b21b56643c1
英士 <comu2e@gmail.com>
2020-12-21 00:16:50 +0900

    Gearテーブルにレポジトリーパターン導入

---
+++ app/Respositories/Gear/GearDataAccessEQRepository.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Repositories\Gear;
+
+use App\Models\Gear;
+use Illuminate\Http\Request;
+
+class GearDataAccessEQRepository implements GearDataAccessRepositoryInterface
+{
+    /**
+     * Gearテーブルからデータをすべて取得
+     * @return Gear[]|\Illuminate\Database\Eloquent\Collection
+     *
+     */
+    public function getAll()
+    {
+        return Gear::all();
+    }
+
+    /**
+     * Gearテーブルから特定のユーザーのギアを取得
+     * updated_atの新しいものから表示するようにする。
+     *
+     * @param $user_id
+     */
+    public function getUser($user_id)
+    {
+
+        return Gear::where('user_id', $user_id)->orderby('updated_at', 'desc')->get();
+
+    }
+
+    /**
+     * @param $category
+     * @return mixed
+     */
+
+    public function getCategorizedGear($category)
+    {
+        return Gear::where('gear_category', $category)->orderby('updated_at', 'desc')->get();
+
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function findAll($id)
+    {
+        return Gear::find($id);
+    }
+
+    public function updateGear(Request $request, Gear $gear)
+    {
+        $gear->update($request->all());
+
+        return $gear;
+    }
+
+    public function UpdateSelect($id, $key, $value)
+    {
+        //すでにwhere　idで指定ししたtaskを更新する。
+        return Gear::where("id", $id)->update([$key => $value]);
+
+    }
+
+    public function deleteSelect($id)
+    {
+
+        return Gear::where('id', $id,)->delete();
+    }
+
+    public function WhereGetSelect($select_key, $id)
+    {
+
+        return Gear::where($select_key, $id,)->get();
+    }
+
+}
---
+++ app/Respositories/Gear/GearDataAccessRepositoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories\Gear;
+
+use App\Models\Gear;
+use DB;
+use Illuminate\Http\Request;
+
+interface GearDataAccessRepositoryInterface
+{
+
+    public function getAll();
+    public function getUser($user_id);
+    public function getCategorizedGear($category);
+    public function findAll($id);
+    public function updateGear(Request $request, Gear $gear);
+    public function deleteSelect($id);
+    public function WhereGetSelect($select_key,$id);
+    public function UpdateSelect($id,$key,$value);
+
+
+}

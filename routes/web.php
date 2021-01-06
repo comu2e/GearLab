7790529f0ef63e0119304fc23a90101112a9efb9
英士 <comu2e@gmail.com>
2021-01-06 21:52:08 +0900

    fix:follow機能のルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -97,14 +97,14 @@ Route::group(['middleware' => 'auth'], function () {
             'only' => ['store'],
         ]);
 
-//フォロー]
-        Route::get('/following/{user_id}', 'FollowUserController@index')->name('follow.index');
-//    Route::get('/users_show/{user_id}/}', 'FollowUserController@show')->name('follow.show');
-        Route::post('/follow-users', 'FollowUserController@store')->name('follow');
-        Route::delete('/follow-users/{user_id}', 'FollowUserController@destroy')->name('unfollow');
-
 
     });
+//フォロー]
+    Route::get('/following/{user_id}', 'FollowUserController@index')->name('follow.index');
+//    Route::get('/users_show/{user_id}/}', 'FollowUserController@show')->name('follow.show');
+    Route::post('/follow-users', 'FollowUserController@store')->name('follow');
+    Route::delete('/follow-users/{user_id}', 'FollowUserController@destroy')->name('unfollow');
+
 
 
 });

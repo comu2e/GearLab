20d7eb1287ea6130a0f749639f694c54259541eb
英士 <comu2e@gmail.com>
2021-01-25 20:06:05 +0900

    バリデーションメッセージの日本語化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '入力されたユーザーIDは登録されていません',
+    'throttle' => 'ログインが多数回行われています。:seconds　分後に再度入力してください。',
+
+];

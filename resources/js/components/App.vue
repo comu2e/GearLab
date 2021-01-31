43b961a5cb87830f01127ad8d14d187da04fda32
英士 <comu2e@gmail.com>
2021-01-31 17:59:49 +0900

    ナビゲーションバー選択しているものがわかるようにする

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,22 +1,22 @@
 <template>
 
     <div class="container mt-5 mb-3">
-        <nav class="navbar navbar-expand-sm  bg-dark navbar-dark justify-content-center sticky-top">
+        <nav class="navbar navbar-expand-sm bg-nav navbar-light justify-content-center sticky-top">
 
             <ul class="navbar-nav">
-                <li class="nav-item">
+                <li class="nav-item-bar" v-on:click="change('1')" v-bind:class="{'active': isActive === '1'}">
                     <router-link :to="{ name: 'home'}" class="nav-link">ギアタイムライン</router-link>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item-bar" v-on:click="change('2')" v-bind:class="{'active': isActive === '2'}">
                     <router-link :to="{ name: 'likes',param:{value:this.$store.state.auth_user}}" class="nav-link">
                         使ってみたいギア
                     </router-link>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item-bar"v-on:click="change('3')" v-bind:class="{'active': isActive === '3'}">
                     <router-link to="/follows" class="nav-link">フォローしているユーザー</router-link>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item-bar"v-on:click="change('4')" v-bind:class="{'active': isActive === '4'}" >
                     <router-link :to="{ name: 'registerd'}" class="nav-link">登録したギア
                     </router-link>
                 </li>
@@ -45,13 +45,22 @@
 export default {
     name: "App",
     props: ['user'],
+    data() {
+        return{
+            isActive: '1'
+        }
+    },
+    methods: {
+        change: function (num) {
+            this.isActive = num
+        }
+    },
     computed: {
         auth_user() {
             return this.$store.state.auth_user;
         }
     },
     mounted() {
-
         this.$store.commit("setter", this.$props.user);
     }
 }
@@ -62,6 +71,21 @@ export default {
     transition: opacity .5s;
 }
 
+bg-nav{
+    color:#353a3f
+}
+a.nav-link.router-link-exact-active.router-link-exact-active{
+    border-bottom: red;
+
+}
+
+li.nav-item-bar.active {
+    color: #fff;
+    font-weight: 700;
+    opacity: 1;
+    border-bottom: solid 2px #0069d9;
+
+}
 .v-enter, .v-leave-to {
     opacity: 0;
 }

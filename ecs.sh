cdaa042499161c4a2c3f88c62d0c56b1f7cae636
英士 <comu2e@gmail.com>
2021-02-01 22:10:14 +0900

    ecs.shの修正

--- ecs.sh
+++ ecs.sh
@@ -1,9 +1,13 @@
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 499506883380.dkr.ecr.ap-northeast-1.amazonaws.com
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+
+docker-compose up -d
 
 docker tag nginx:latest 499506883380.dkr.ecr.ap-northeast-1.amazonaws.com/nginx:latest
 docker tag php:latest 499506883380.dkr.ecr.ap-northeast-1.amazonaws.com/php:latest
 docker push 499506883380.dkr.ecr.ap-northeast-1.amazonaws.com/nginx:latest
 docker push 499506883380.dkr.ecr.ap-northeast-1.amazonaws.com/php:latest
 
-cd /Desktop/AWS
+cd ~/Desktop/AWS
 ssh -i "mykey.pem" ec2-user@ec2-54-249-78-119.ap-northeast-1.compute.amazonaws.com

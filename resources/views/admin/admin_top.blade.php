b17045015e18dfb136fbe33b883d9614d5b37948
英士 <comu2e@gmail.com>
2021-02-14 16:50:14 +0900

    duskでのテスト追加 Create画面への遷移をduskで検証

--- resources/views/admin/admin_top.blade.php
+++ resources/views/admin/admin_top.blade.php
@@ -6,7 +6,7 @@
             <div class="card-header">管理側TOP</div>
             <div class="card-body">
                 <div>
-                    <a href="{{ url('admin/user_list') }}" class="btn btn-primary">ユーザー一覧</a>
+                    <a href="{{ url('admin/user_list') }}" class="btn btn-primary" >ユーザー一覧</a>
                 </div>
 
                 <form method="post" action="{{ url('admin/logout') }}">
